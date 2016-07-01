371048b679006579a02f6edf5ae1bde97834221e
ezeperezcrack <ezequielda@outlook.com>
2016-07-01 20:09:18 -0300

    este es mi segundo commit

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,7 +3,7 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
+{	alert("sumar")
 	var numerouno= document.getElementById('numeroUno').value;
 var numerodos= document.getElementById('numeroDos').value;
 var resultado= parseInt(numerouno)+parseInt(numerodos);
